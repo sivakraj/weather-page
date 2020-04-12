04dd1e4d095bfee825d050f3205fc9d2fc0ec4a9
Sivakumar Rajendran <emails2shiva@gmail.com>
2020-04-12 21:42:09 +0530

    Changes include the server and client code to fetch weather and dynamically add it to the UI

---
+++ server.js
@@ -0,0 +1,62 @@
+// Setup empty JS object to act as endpoint for all routes
+projectData = {};
+
+// Require Express to run server and routes
+const express = require('express');
+
+// Start up an instance of app
+const app = express();
+
+/* Middleware*/
+//Here we are configuring express to use body-parser as middle-ware.
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
+
+// Initialize the main project folder
+app.use(express.static('website'));
+
+//Setup port for the server
+const port = 3000;
+
+// Spin up the server
+const server = app.listen(port,
+    () => console.log(`Server started and listening to the port: ${port}`));
+
+// Initialize all route with a callback function
+app.get('/all', sendProjectData);
+
+// Callback function to complete GET '/all'
+/**
+ * @description Sends all routes end point 'projectData' to client
+ * @param {Object} request - contains the data passed from client
+ * @param {Object} response - response to be sent back to client
+ */
+function sendProjectData(request, response) {
+    response.send(projectData);
+}
+
+// Post Route
+app.post('/addWeatherData', addWeatherData);
+
+/**
+ * @description Adds data fetched from OpenWeatherMap and user entered data to the all routes end point 'projectData'
+ * @param {Object} request - contains the data passed from client
+ * @param {Object} response - response to be sent back to client
+ */
+function addWeatherData(request, response) {
+
+    newData = {
+        temperature: request.body.temperature,
+        date: request.body.date,
+        userData: request.body.userData
+    };
+
+    projectData.weather = newData;
+
+    response.send(newData);
+}
---
+++ website/app.js
@@ -0,0 +1,114 @@
+/* Global Variables */
+
+// Create a new date instance dynamically with JS
+let d = new Date();
+let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+// Personal API Key for OpenWeatherMap API
+const OPEN_WEATHER_API_KEY = 'b9b497f66799fa7ee3cdd5770a142f0c';
+
+const BASE_WEATHER_URL = 'https://api.openweathermap.org/data/2.5/weather?zip=';
+
+//Authorization to append API Key
+const AUTH = '&appid=';
+
+//Part of data used to convert the temperature from Kelvin to F
+const KELVIN_CONV_VALUE = 459.67;
+
+//Data to display when OpenWeatherMao returns error
+const ERROR_TEMP_FETCH = 'Error in fetching data.';
+
+// Event listener to add function to existing HTML DOM Button element
+document.getElementById('generate').addEventListener('click', generateWeather);
+
+/* Function called by event listener */
+/**
+ * @description Call back function called on click of Generate button
+ * @param {Event object} event - browser events - click in this case
+ */
+function generateWeather(event) {
+    const zip = document.getElementById('zip').value;
+    const feelings = document.getElementById('feelings').value;
+
+    //Bail out early if zip code is not entered or it's not a number
+    //Chosen global isNaN() rather than Number.isNaN() intentionally
+    if(isNaN(zip)) {
+        alert('Enter a zip code to continue');
+        return;
+    }
+
+    //Call GET Web API
+    getData(`${BASE_WEATHER_URL}${zip}${AUTH}${OPEN_WEATHER_API_KEY}`)
+    .then((data) => {
+        if(data.cod == 200) { //for success response
+            postData('/addWeatherData', {temperature: getTempInF(data.main.temp), date: newDate, userData: feelings});
+        } else { //for error scenarios
+            postData('/addWeatherData', {temperature: ERROR_TEMP_FETCH, date: newDate, userData: feelings});
+        }
+        updateUI();
+    });
+}
+
+/* Function to GET Web API Data*/
+/**
+ * @description Makes a GET call to the URL specified and returns the response
+ * @param {String} url - URL to make a GET call to
+ */
+const getData = async (url = '') => {
+    const request = await fetch(url);
+    try{
+        const response = await request.json();
+        return response;
+    } catch(error) {
+        console.error('Error in fetching data: ', error);
+    }
+};
+
+/* Function to POST data */
+/**
+ * @description Makes a POST call to the URL specified along with properties and data
+ * @param {String} url - URL to make a POST call to
+ * @param {Object} data - data to be posted to server
+ */
+const postData = async (url = '', data = {}) => {
+    const response = await fetch(url, {
+        method: 'POST',
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    });
+
+    try {
+        const newData = await response.json();
+        return newData;
+    } catch (error) {
+        console.error('Error in posting data: ', error);
+    }
+};
+
+/* Function to GET Project Data */
+/**
+ * @description Makes a GET call to get all end point object data and update the same in the UI
+ */
+const updateUI = async () => {
+    const request = await fetch('/all');
+
+    try {
+        const projectData = await request.json();
+        document.getElementById('temp').innerHTML = projectData.weather.temperature;
+        document.getElementById('date').innerHTML = projectData.weather.date;
+        document.getElementById('content').innerHTML = projectData.weather.userData;
+    } catch (error) {
+        console.log('Error in updating UI: ', error);
+    }
+};
+
+/**
+ * @description Converts temperature in Kelvin to F
+ * @param {String} temp - value in Kelvin that needs to be converted to F and returned to the calling method
+ */
+function getTempInF(temp) {
+    return Math.round(temp * (9/5) - KELVIN_CONV_VALUE);
+}
